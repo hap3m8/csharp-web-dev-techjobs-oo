0ccad62d757aa7414068a24018720fd8c75fd85d
hap3m8 <56phillips58@gmail.com>
2021-01-05 21:47:46 -0600

    * Job.cs: * JobField.cs: * Employer.cs: * Location.cs: * PositionType.cs: * CoreCompetency.cs:
    * Program.cs: classes/support classes completed

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TechJobsOO
 {
     public class Job
@@ -14,6 +16,69 @@ namespace TechJobsOO
 
         // TODO: Add the two necessary constructors.
 
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+
+        }
+
+
         // TODO: Generate Equals() and GetHashCode() methods.
+
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            string stringId = Id.ToString();
+            string combinedJobListing = "";
+
+            Dictionary<string, string> fields = new Dictionary<string, string>
+            {
+                {"ID", stringId},
+                {"Name", Name},
+                {"Employer", EmployerName.ToString()},
+                {"Location", EmployerLocation.ToString()},
+                {"Position Type", JobType.ToString()},
+                {"Core Competency", JobCoreCompetency.ToString()},
+            };
+
+            foreach (KeyValuePair<string, string> field in fields)
+            {
+
+                string isFieldNull;
+
+                if (field.Value == "")
+                {
+                    isFieldNull = "Data not available";
+                }
+                else
+                {
+                    isFieldNull = field.Value;
+                }
+
+                combinedJobListing += field.Key + ": " + isFieldNull + System.Environment.NewLine;
+            }
+
+            return System.Environment.NewLine + combinedJobListing;
+        }
     }
 }
